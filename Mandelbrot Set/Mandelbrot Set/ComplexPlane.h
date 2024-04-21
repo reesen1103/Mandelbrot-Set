7784f0433df1130a0e3422fa9d3479b681da1bca
emanculi <158606682+emanculi@users.noreply.github.com>
2024-04-21 15:39:18 -0700

    ComplexPlane.h by the instructions of professor
    if i did something wrong just let me know

--- Mandelbrot Set/Mandelbrot Set/ComplexPlane.h
+++ Mandelbrot Set/Mandelbrot Set/ComplexPlane.h
@@ -1,1 +1,44 @@
-#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <complex>
+#include <valarray>
+#include <cstdlib>
+#include <cmath>
+
+const unsigned int MAX_ITER = 64;
+const float BASE_WIDTH = 4.0;
+const float BASE_HEIGHT = 4.0;
+const float BASE_ZOOM = 0.5;
+
+// Make code easier to type with "using namespace"
+using namespace sf;
+using namespace std;
+
+enum class State(CALCULATING, DISPLAYING)
+{
+
+	ComplexPlane(int pixelWidth, int pixelHeight);
+
+	void draw(RenderTarget & target, RenderStates states);
+
+	void updateRender();
+
+	void zoomIn();
+
+	void zoomOut();
+
+	void setCenter(Vector2i mousePixel);
+
+	void loadText(Text& text);
+
+	int size_t countIterations(Vector2f coord);
+
+	void iterationsToRGB(size_t cont, Uint8& r, Uint8& g, Uint8& b);
+
+	Vector2f mapPixelToCoord(Vector2i mousePixel);
+
+
+};
