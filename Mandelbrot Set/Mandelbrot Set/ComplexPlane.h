20fc5fbad2491bc67f27ec2fd313a2ca21b66fe0
reesen1103 <166560805+reesen1103@users.noreply.github.com>
2024-04-24 18:39:48 -0700

    Update ComplexPlane.h
    complexPlane.h should work as intended

--- Mandelbrot Set/Mandelbrot Set/ComplexPlane.h
+++ Mandelbrot Set/Mandelbrot Set/ComplexPlane.h
@@ -1,5 +1,6 @@
-#ifndef STATE_H
-#define STATE_H
+#pragma once
+#ifndef COMPLEXPLANE_H
+#define COMPLEXPLANE_H
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <iostream>
@@ -19,33 +20,32 @@ const float BASE_ZOOM = 0.5;
 using namespace sf;
 using namespace std;
 
-enum class State(CALCULATING, DISPLAYING)
+enum class State { CALCULATING, DISPLAYING };
+
+class ComplexPlane : public Drawable 
 {
 public:
 	ComplexPlane(int pixelWidth, int pixelHeight);
-	void draw(RenderTarget & target, RenderStates states);
+	void draw(RenderTarget& target, RenderStates states) const override;
 	void updateRender();
-	void zoomIn();
-	void zoomOut();
+	void zoomIn(int x, int y);
+	void zoomOut(int x, int y);
+	void setMouseLocation(int x, int y);
 	void setCenter(Vector2i mousePixel);
 	void loadText(Text& text);
-	int size_t countIterations(Vector2f coord);
-	void iterationsToRGB(size_t cont, Uint8& r, Uint8& g, Uint8& b);
+	int countIterations(Vector2f coord);
+	void iterationsToRGB(int cont, Uint8& r, Uint8& g, Uint8& b);
 	Vector2f mapPixelToCoord(Vector2i mousePixel);
 
 private:
 
-	VertexArray m_pixel_size;
+	VertexArray m_pixelArray;
 	State m_state;
 	Vector2f m_mouseLocation;
-	Vector2i m_pixel_size;
-	Vector2f m_plane_center;
-	Vector2f m_plane_size;
+	Vector2i m_pixelSize;
+	Vector2f m_planeCenter;
+	Vector2f m_planeSize;
 	int m_zoomCount;
 	float m_aspectRatio;
-
-
-
-
 };
 #endif
