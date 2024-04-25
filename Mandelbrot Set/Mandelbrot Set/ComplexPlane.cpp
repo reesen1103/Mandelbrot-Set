e897af294bcec2b27ca3b7085899ead347f21e50
reesen1103 <166560805+reesen1103@users.noreply.github.com>
2024-04-24 18:38:40 -0700

    Update ComplexPlane.cpp
    draw, update render, zoom in, zoom out, set center, and load text should all be complete. need to finish the rest to find out

--- Mandelbrot Set/Mandelbrot Set/ComplexPlane.cpp
+++ Mandelbrot Set/Mandelbrot Set/ComplexPlane.cpp
@@ -8,88 +8,119 @@
 #include <cstdlib>
 #include <cmath>
 
-#include "ComplexPlane.h";
+#include "ComplexPlane.h"
 
-ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
+using namespace std;
+using namespace sf;
+
+ComlexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
 {
 	m_aspectRatio = (pixelWidth) / pixelHeight;
-	m_plane_center = {0,0};
-	m_plane_size = {BASE_WIDTH, BASE_HEIGHT * m_aspectRatio};
+	m_planeCenter = { 0,0 };
+	m_planeSize = { BASE_WIDTH, BASE_HEIGHT * m_aspectRatio };
 	m_zoomCount = 0;
-	m_State = State::CALCULATING;
-	VertexArray = Points; // Resize it to pixelWidth* pixelHeight??
-	return 0;
+	m_state = State::CALCULATING;
+	//VertexArray = Points; // Resize it to pixelWidth* pixelHeight??
+	m_pixelArray.resize(pixelWidth * pixelHeight);
 }
 
 void ComplexPlane::draw(RenderTarget& target, RenderStates states)
 {
-	if (m_State == CALCULATING)
+	if (m_state == State::CALCULATING)
 	{
-		for
+		for (int i = 0; i < m_pixelArray.getVertexCount(); i++)
+		{
+			float x = m_pixelArray[i].position.x; // get pixel coordinates
+			float y = m_pixelArray[i].position.y;
+
+			Vector2f coord = mapPixelToCoord(Vector2i(x, y)); // map pixel coords to plane
+
+			int iterations = countIterations(coord);
+			Uint8 r, g, b;
+			iterationsToRGB(iterations, r, g, b); //map iterations to RGB color
+
+			m_pixelArray[i].color = Color(r, g, b); // update pixel color
+		}
+		m_state = State::DISPLAYING; //set state to displaying after setting the pixel colors
 	}
-	target.draw(m_vArray);
-	return 0;
+	target.draw(m_pixelArray, states); //draw pixel array
 }
 
 void ComplexPlane::updateRender()
 {
-	return 0;
+	if (m_state == State::CALCULATING)
+	{
+		for (int i = 0; i < m_pixelArray.getVertexCount(); i++)
+		{
+			float x = m_pixelArray[i].position.x; // get pixel coordinates
+			float y = m_pixelArray[i].position.y;
+
+			Vector2f coord = mapPixelToCoord(Vector2i(x, y)); // map pixel coords to plane
+
+			int iterations = countIterations(coord);
+			Uint8 r, g, b;
+			iterationsToRGB(iterations, r, g, b); //map iterations to RGB color
+
+			m_pixelArray[i].color = Color(r, g, b); // update pixel color
+		}
+		m_state = State::DISPLAYING; //set state to displaying after setting the pixel colors
+	}
 }
 
-void ComplexPlane::zoomIn()
+void ComplexPlane::zoomIn() // all the code here was fine for the most part, just a couple of misnamed variables.
 {
 	m_zoomCount++;
-	int x = BASE_WIDTH * (pow(BASE_ZOOM, m_ZoomCount));
-	int y = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_ZoomCount));
-	m_plane_size(x, y);
-	m_State(CALCULATING);
-	return 0;
+	float newWidth = BASE_WIDTH * pow(BASE_ZOOM, m_zoomCount);
+	float newHeight = BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_zoomCount);
+	m_planeSize = { newWidth, newHeight };
+	m_state = State::CALCULATING;
 }
 
-void ComplexPlane::zoomOut()
+void ComplexPlane::zoomOut() // same thing here
 {
 	m_zoomCount--;
-	int x = BASE_WIDTH * (pow(BASE_ZOOM, m_ZoomCount));
-	int y = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_ZoomCount));
-	m_plane_size(x, y);
-	m_State(CALCULATING);
-	return 0;
+	float newWidth = BASE_WIDTH * pow(BASE_ZOOM, m_zoomCount);
+	float newHeight = BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_zoomCount);
+	m_planeSize = { newWidth, newHeight };
+	m_state = State::CALCULATING;
 }
 
-void ComplexPlane::setCenter(Vector2i mousePixel)
+void ComplexPlane::setCenter(Vector2i mousePixel) // same here, just needed to assign m_mouseLocation and m_state variables
 {
-	m_mouseLocation(ComplexPlane::mapPixelToCoords(Vector2f)); 
-	m_State(CALCULATING);
-	return 0;
+	m_mouseLocation = ComplexPlane::mapPixelToCoord(mousePixel);
+	m_state = State::CALCULATING;
 }
 
 //creates the output which shows coordinates
 //and instructions to the game
-void ComplexPlane::loadText(Text& text)   
+void ComplexPlane::loadText(Text& text) // replaced '<<' and endl with '\n' inside of setString functions. we want to display these to the user, not output to the terminal
 {
-	text << "Mandelbrot Set " << endl;
-	text << "Center: " << endl; //will show the user the center of the set
-	text << "Cursor: " << endl; //will show the user where the cursor is pointing in the plane
-	text << "Left-click to Zoom in" << endl;
-	text << "Richt-click to Zoom out" << endl;
-
-	return *this;
+	text.setString("Mandelbrot Set\n");
+	text.setString("Center: (" + std::to_string(m_planeCenter.x) + ", " + std::to_string(m_planeCenter.y) + ")\n");
+	text.setString("Cursor: (" + std::to_string(m_mouseLocation.x) + ", " + std::to_string(m_mouseLocation.y) + ")\n");
+	text.setString("Left-click to Zoom in\n");
+	text.setString("Right-click to Zoom out\n");
 }
 
 
 //will count the number of iterations of the set for the given coordinate 
-int size_t ComplexPlane::countIterations(Vector2f coord)
+int ComplexPlane::countIterations(Vector2f coord)
 {
-	return 0;
+	//Count the number of iterations of the set for the given coordinate as specified above
 }
 
 
 void iterationsToRGB(size_t cont, Uint8& r, Uint8& g, Uint8& b)
 {
-	return 0;
+	//see instructions
 }
 
 Vector2f mapPixelToCoord(Vector2i mousePixel)
 {
-	return 0;
+	//see instructions
+}
+void ComplexPlane::setMouseLocation(int x, int y)
+{
+	m_mouseLocation.x = x;
+	m_mouseLocation.y = y;
 }
